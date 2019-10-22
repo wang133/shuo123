4af7cd9dd261234b3bd06d325385bddd864a7119
wang133 <1462145054@qq.com>
2019-10-22 17:17:33 +0800

    .33

---
+++ 216.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (void)
+{
+    int time = 3, distance;
+    printf("Enter time:\n");
+    distance = 1.0 / 2 * 10 * time * time;
+    printf("Distance is %d\n",distance);
+    return 0;
+}
